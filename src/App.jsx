e1ccecc8ab833b65f1f19efe590fad4f8d62babe
abhir1 <abhir4702@gmail.com>
2023-12-31 20:16:58 +0530

    INCREASE THE SIZE OF CALCULATOR

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
   return (
     <>
       <div className="calculator" >
+        <div className='cal-name'>Abhishek calculator</div>
         <Display displayValue={calVal}></Display>
         <Buttons onButtonClick={onButtonClick} ></Buttons>
         
